0a520e384e599dfb04504eb01abde56bacdd38dc
ashitakhare <ashitakhare@gmail.com>
2017-03-21 21:24:51 +0530

    Add files via upload

---
+++ APM - Start/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector:'pm-app',
+    template:'<pm-products></pm-products>'
+    
+
+})
+
+export class AppComponent{
+    page_heading: string = 'My First AppComponent';
+
+}
